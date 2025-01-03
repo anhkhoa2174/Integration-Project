d61790703357a59f6bb1aae77b3de5e58f5fa80a
Duy <duy.nguyenacht@hcmut.edu.vn>
2025-01-03 22:15:02 +0700

    Navbar + Sidebar

--- react-webpack/public/backend/app.py
+++ react-webpack/public/backend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash, send_file
 from functools import wraps
-import psycopg2
+# import psycopg2
 from logging.config import dictConfig
 import logging
 import sys
 import base64
 import os
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 from io import BytesIO
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -15,9 +15,9 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 file_storage = {}
 
-# Kết nối cơ sở dữ liệu
-def get_db_connection():
-    return psycopg2.connect(database="CNPM", user="postgres", password="p123", host="localhost", port="5432")
+# # Kết nối cơ sở dữ liệu
+# def get_db_connection():
+#     return psycopg2.connect(database="CNPM", user="postgres", password="p123", host="localhost", port="5432")
 
 
 @app.route('/')
@@ -35,6 +35,26 @@ def transaction_history():
 @app.route('/deposit')
 def deposit():
     return render_template('deposit.html')
+
+@app.route('/dashboard')
+def admin_dashboard():
+    return render_template('administrator/dashboard.html')
+
+@app.route('/navbar')
+def admin_navbar():
+    return render_template('administrator/navbar.html')
+
+@app.route('/sidebar')
+def admin_sidebar():
+    return render_template('administrator/sidebar.html')
+
+@app.route('/navsidebar')
+def admin_navsidebar():
+    return render_template('administrator/navsidebar.html')
+
+@app.route('/test')
+def admin_test():
+    return render_template('administrator/test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
